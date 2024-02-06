afecc9cc2e782b67a61565ca1641f4ecf3d7afaf
Shilpa465 <150187235+Shilpa465@users.noreply.github.com>
2024-02-06 14:04:07 -0500

    list user front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Route, Swtich} from 'react-router-dom';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div className="container">
+          <HeaderComponent />
+            <div className="container">
+              <Switch> 
+                <Route path = "/" component = {ListEmployeeComponent}></Route>
+                <Route path ="/employees" component = {ListEmployeeComponent}></Route>
+                <Route path = "/employees" component = {CreateEmployeeComponent}></Route>
+              </Switch>
+            </div>
+          <FooterComponent />
+        </div>
+      </Router>
     </div>
+
+
   );
 }
 
